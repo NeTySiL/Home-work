45387f9711de244acfef8d0e30c86f33af937035
Alexey Kononchenko <alexey.kononchenko@gmail.com>
2015-10-15 17:27:28 +0300

    First OOP

---
+++ OOP/TestDemo.java
@@ -0,0 +1,13 @@
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		Abiturient [] listAbiturients = new Abiturient[6];
+		listAbiturients [0] = new Abiturient(000051, "Dahnevich", "Valentina", "Alexandrovna", "Minsk", 336203650, 5);
+		listAbiturients [1] = new Abiturient(000054, "Gavniluk", "Mihail", "Ipolitovich", "Gomel", 296489234, 2);
+		listAbiturients [2] = new Abiturient(000053, "Kononchenko", "Elena", "Sergeevna", "Lida", 294507853, 4);
+		listAbiturients [3] = new Abiturient(000052, "Mishenko", "Sergey", "Olegovich", "Soligorsk", 336707089, 4);
+		listAbiturients [4] = new Abiturient(000055, "Gus", "Artem", "Nikolaevich", "Minsk", 294560987, 3);
+		listAbiturients [5] = new Abiturient(000056, "Popov", "Inakentiy", "Jakovlevich", "Brest", 259805050, 5);
+	}
+}
